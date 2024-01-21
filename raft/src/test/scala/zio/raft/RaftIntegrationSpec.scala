80052b7ca2484515e9e885bc2d2660ddb894991c
Shaked Kaplan <shaked@unit.co>
2024-01-21 14:32:21 +0200

    add more tests

--- raft/src/test/scala/zio/raft/RaftIntegrationSpec.scala
+++ raft/src/test/scala/zio/raft/RaftIntegrationSpec.scala
@@ -1,15 +1,22 @@
 package zio.raft
 
+import zio.ZIO
+import zio.duration.*
+import zio.logging.LogFormat
+import zio.logging.LogLevel
+import zio.logging.Logging
 import zio.test.DefaultMutableRunnableSpec
 import zio.test.*
-import zio.ZIO
-import zio.logging.Logging
-import zio.logging.LogLevel
-import zio.logging.LogFormat
-import zio.duration.*
 
 object RaftIntegrationSpec extends DefaultMutableRunnableSpec:
     val logging = Logging.console(logLevel = LogLevel.Debug, format = LogFormat.ColoredLogFormat()) >>> zio.logging.Logging.withRootLoggerName("raft")
+    private def findTheLeader(raft1: Raft[TestCommands], raft2: Raft[TestCommands], raft3: Raft[TestCommands]) =
+        for 
+            r1IsLeader <- raft1.isTheLeader
+            r2IsLeader <- raft2.isTheLeader
+            r3IsLeader <- raft3.isTheLeader
+        yield if r1IsLeader then raft1 else if r2IsLeader then raft2 else if r3IsLeader then raft3 else throw new Exception("no leader")
+
     private def makeRaft =
         for 
             rpc <- TestRpc.make[TestCommands](3).toManaged_
@@ -54,17 +61,11 @@ object RaftIntegrationSpec extends DefaultMutableRunnableSpec:
         yield (raft1, rpc(0)._1, rpc(0)._2, raft2, rpc(1)._1, rpc(1)._2 , raft3, rpc(2)._1, rpc(2)._2)
     testM("raft"):
         for       
-            _ <- ZIO.unit
             res <- makeRaft.use{
             case (r1,rpc1, killSwitch1, r2, rpc2, killSwitch2, r3, rpc3, killSwitch3) => {
                 for 
-                    _ <- ZIO.unit
-                    
                     _ <- ZIO.sleep(5.seconds).provideLayer(zio.clock.Clock.live)
-                    r1IsLeader <- r1.isTheLeader.debug("r1 is leader")
-                    r2IsLeader <- r2.isTheLeader.debug("r2 is leader")
-                    r3IsLeader <- r3.isTheLeader.debug("r3 is leader")
-                    leader = if r1IsLeader then r1 else if r2IsLeader then r2 else if r3IsLeader then r3 else throw new Exception("no leader")
+                    leader <- findTheLeader(r1, r2, r3)
                     _ <- leader.sendCommand(Increase)
                     _ <- leader.sendCommand(Increase)
                     _ <- leader.sendCommand(Increase)
@@ -73,4 +74,48 @@ object RaftIntegrationSpec extends DefaultMutableRunnableSpec:
             }
             }.provideLayer(zio.ZEnv.live ++ logging)
         yield assertTrue(res == 3)
-  
+    
+    testM("raft, make sure consensus is reached even when 1 node is disconnected"):
+        for       
+            res <- makeRaft.use{
+                case (r1,rpc1, killSwitch1, r2, rpc2, killSwitch2, r3, rpc3, killSwitch3) => {
+                for 
+                    _ <- ZIO.sleep(5.seconds).provideLayer(zio.clock.Clock.live)
+                    _ <- killSwitch1.set(false)
+                    leader <- findTheLeader(r1, r2, r3)
+                    _ <- leader.sendCommand(Increase)
+                    _ <- leader.sendCommand(Increase)
+                    _ <- leader.sendCommand(Increase)
+                    x <- leader.sendCommand(Get)
+                yield (x)
+            }
+            }.provideLayer(zio.ZEnv.live ++ logging)
+        yield assertTrue(res == 3)
+            
+                
+    testM("raft, make sure leader is replaced") {
+            for       
+                res <- makeRaft.use{
+                    case (r1,rpc1, killSwitch1, r2, rpc2, killSwitch2, r3, rpc3, killSwitch3) => {
+                    for 
+                        _ <- ZIO.sleep(5.seconds).provideLayer(zio.clock.Clock.live)
+                        _ <- killSwitch1.set(false)
+                        beforeLeader <- findTheLeader(r1, r2, r3)
+                        r1IsLeader <- r1.isTheLeader.debug("r1 is leader")
+                        r2IsLeader <- r2.isTheLeader.debug("r2 is leader")
+                        r3IsLeader <- r3.isTheLeader.debug("r3 is leader")
+                        //stop the leader
+                        _ <- if r1IsLeader then killSwitch1.set(false) else if r2IsLeader then killSwitch2.set(false) else if r3IsLeader then killSwitch3.set(false) else throw new Exception("no leader")
+                        _ <- ZIO.sleep(20.seconds).provideLayer(zio.clock.Clock.live)
+                        // find the new leader
+                        leader <- findTheLeader(r1, r2, r3).repeatWhile(_ == beforeLeader)
+                        _ <- leader.sendCommand(Increase)
+                        _ <- leader.sendCommand(Increase)
+                        _ <- leader.sendCommand(Increase)
+                        x <- leader.sendCommand(Get)
+                    yield (x)
+                }
+                }.provideLayer(zio.ZEnv.live ++ logging)
+            yield assertTrue(res == 3)
+    } @@ TestAspect.ignore
+

0f5703a0459e03fa19a2735e0851ddf34a05aa65
keisar <keisar@users.noreply.github.com>
2025-07-16 18:04:29 +0300

    clean up

--- raft/src/main/scala/zio/raft/PendingReads.scala
+++ raft/src/main/scala/zio/raft/PendingReads.scala
@@ -13,11 +13,9 @@ case class PendingReadEntry[S](
 
 case class PendingReads[S](entries: List[PendingReadEntry[S]]):
   def withAdded(entry: PendingReadEntry[S]): PendingReads[S] =
-    // Optimize for the common case: new entry has higher index than all existing entries
     if (entries.isEmpty || entry.enqueuedAtIndex >= entries.last.enqueuedAtIndex) then
       PendingReads(entries :+ entry)
     else
-      // Find the correct insertion point to maintain sorted order
       val (before, after) = entries.span(_.enqueuedAtIndex <= entry.enqueuedAtIndex)
       PendingReads(before ++ (entry :: after))
 
